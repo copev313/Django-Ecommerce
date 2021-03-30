a915c025bab3f6d07034e843c04be744462f0bd6
Evan Cope <copev313@gmail.com>
2021-03-29 21:23:18 -0400

    started default app + server

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
