e65f9e24abbd3b6fae5530d64820cfa232d414e8
Evan Cope <copev313@gmail.com>
2021-04-03 21:11:39 -0400

    added static files directory + media root & URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,6 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 CORS_ALLOW_ALL_ORIGINS = True
