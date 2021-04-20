9575e658df119a1332203f908eb0e04052f116e9
Evan Cope <copev313@gmail.com>
2021-04-19 20:52:26 -0400

    added Cart reducer & action + LocalStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-import { 
-    productListReducer, 
-    productDetailsReducer,
-
-} from './reducers/productReducers';
+import {  productListReducer,  productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-});
+    cart: cartReducer,
+})
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+        JSON.parse(localStorage.getItem('cartItems')) : []
+
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage },
+}
 
 const middleware = [thunk]
 
 const store = createStore(reducer, initialState, 
-    composeWithDevTools(applyMiddleware(...middleware)));
+                composeWithDevTools(applyMiddleware(...middleware)))
 
-export default store;
+export default store
