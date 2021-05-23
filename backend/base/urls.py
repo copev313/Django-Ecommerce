eabca7a4d0c194f3a33133f42e022c4c5492c56e
Evan Cope <copev313@gmail.com>
2021-05-23 16:15:05 -0400

    added simple JWT for Django REST Framework

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from . import views
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
+
 ]
