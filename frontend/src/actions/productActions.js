e9f641dd13d2d7911f2493aa0ab5ffc77f19c4c7
Evan Cope <copev313@gmail.com>
2021-05-25 20:37:39 -0400

    state management for user actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -27,8 +27,8 @@ export const listProducts = () => async (dispatch) => {
     catch (error) {
         dispatch({ 
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message ?
-                error.response.data.message : error.message ,
+            payload: error.response && error.response.data.detail ?
+                error.response.data.detail : error.message,
         });
     }
 }
@@ -49,8 +49,8 @@ export const listProductDetails = (id) => async (dispatch) => {
     catch (error) {
         dispatch({ 
             type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.message ?
-                error.response.data.message : error.message ,
+            payload: error.response && error.response.data.detail ?
+                error.response.data.detail : error.message,
         });
     }
 }
