98ca4f67d980a7028a602d520a1502137b3c8a53
Evan Cope <copev313@gmail.com>
2021-06-15 20:13:28 -0400

    fixed issue with Order model

--- backend/base/models.py
+++ backend/base/models.py
@@ -36,7 +36,7 @@ class Review(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
     taxPrice = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
