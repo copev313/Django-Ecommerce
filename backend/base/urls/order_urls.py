75b970b056cb350ee89f034d7984e64ea6b47a2a
Evan Cope <copev313@gmail.com>
2021-06-17 18:52:16 -0400

    quick url fix

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -7,5 +7,5 @@ from base.views import order_views as views
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),
     path('<str:pk>/', views.getOrderById, name='user-order'),
-    path('<str:pk>/pay/', views.updateOrderToPaid(), name='pay'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
 ]
