a3f3099bd9b26102155d78d0751d5f70ca440796
Evan Cope <copev313@gmail.com>
2021-06-21 20:38:12 -0400

    added user list constants, actions, and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,6 +16,7 @@ import {
     userRegisterReducer,
     userDetailsReducer,
     userUpdateProfileReducer,
+    userListReducer,
 
 } from './reducers/userReducers';
 import {
@@ -35,6 +36,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
