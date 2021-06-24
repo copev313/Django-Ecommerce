6e26db91f52677fd0af673ee5ce57eac91a632ca
Evan Cope <copev313@gmail.com>
2021-06-24 19:37:41 -0400

    added createProduct & updateProduct endpoints

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,8 +5,12 @@ from base.views import product_views as views
 
 
 urlpatterns = [
+
+    path('create/', views.createProduct, name='product-create'),
+
     path('', views.getProducts, name='products'),
     path('<str:pk>/', views.getProduct, name='product'),
-    
+
+    path('update/<str:pk>/', views.updateProduct, name='product-update'),
     path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
 ]
