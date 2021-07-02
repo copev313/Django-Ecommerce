66fa22589fe37fe248272733a571a212d6179008
Evan Cope <copev313@gmail.com>
2021-07-02 14:48:30 -0400

    ran collectstatic + debugged deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 
+ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'https://boutsacado.herokuapp.com/',
+    'boutsacado.herokuapp.com',
 ]
 
 # Application definition
